04fc68e0a907c7de90b145d8628e412c8ecfb0ff
S.Hummam Shah <humamshah11@gmail.com>
2020-10-27 20:45:01 +0500

    Initialization of project

---
+++ DareZameen_Web/Controllers/WebController.cs
@@ -0,0 +1,26 @@
+﻿using DareZameen_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DareZameen_Web.Controllers
+{
+    public class WebController : ApiController
+    {
+        private RealStateDataEntities db = new RealStateDataEntities();
+        private static List<string> Customers = new List<string> { "Hummam", "Umer", "Ashok" };
+        public IEnumerable<string> GetName()
+        {
+            var Cust = new List<string>();
+            var DbCust = db.Customer.Take(15).ToList();
+            foreach (var c in DbCust)
+            {
+                Cust.Add(c.Name);
+            }
+            return Cust;
+        }
+    }
+}
