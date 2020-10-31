a1f69d23beec48b781f8f8c5188f5a867743b395
S.Hummam Shah <humamshah11@gmail.com>
2020-10-31 19:00:45 +0500

    ApiFolder created with slight changes in layout file. Home/index content removed with ComingSoon  text

---
+++ DareZameen_Web/Controllers/Api/InventoryApiController.cs
@@ -0,0 +1,29 @@
+﻿using DareZameen_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DareZameen_Web.Controllers.Api
+{
+    public class InventoryApiController : ApiController
+    {
+        // GET api/<controller>
+        private RealStateDataEntities db = new RealStateDataEntities();
+        private static List<string> Customers = new List<string> { "Hummam", "Umer", "Ashok" };
+        [HttpGet]
+        public IEnumerable<string> GetName()
+        {
+            var Cust = new List<string>();
+            var DbCust = db.Customer.Take(15).ToList();
+            foreach (var c in DbCust)
+            {
+                Cust.Add(c.Name);
+            }
+            return Cust;
+        }
+       
+    }
+}
