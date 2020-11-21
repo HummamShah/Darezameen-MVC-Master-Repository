d23cb05ebd9a526ee13293e830ad5999eb9cab17
HummamShah <humamshah11@gmail.com>
2020-11-21 18:38:26 +0500

    Added Db in project

---
+++ DareZameen.Database/dbo/Tables/Customer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (MAX) NULL,
+    [Contact1] NVARCHAR (MAX) NULL,
+    [Contact2] NVARCHAR (MAX) NULL,
+    [Address]  NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
